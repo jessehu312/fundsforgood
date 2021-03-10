dddf00f6349a2b363f544e9d626d61b40709ed47
aryanvichare <aryanvichare10@gmail.com>
2021-03-09 18:21:14 -0800

    Add Dashboard Navigation

---
+++ components/dashboard/Navbar.jsx
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from "react";
+import { useAuth } from "@/lib/auth";
+
+const Navbar = () => {
+  const auth = useAuth();
+
+  return (
+    <div className='w-full flex flex-row justify-between items-center max-w-screen-xl mx-auto py-8 px-4'>
+      <div className='flex flex-row space-x-4 items-center ml-auto'>
+        <img
+          className='w-8 h-8 rounded-full'
+          src={auth?.user?.photoUrl}
+          alt={auth?.user?.name}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
